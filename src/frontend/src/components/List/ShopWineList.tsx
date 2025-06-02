b9545ffcf6490bb6ccc633dcd3da6b9aca722d17
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-06-02 16:10:25 +0200

    update backend

---
+++ src/frontend/src/components/List/ShopWineList.tsx
@@ -0,0 +1,3 @@
+export const ShopWineList = () => {
+  return <></>;
+};
---
+++ src/frontend/src/components/Modal/ShopWineModal.tsx
@@ -0,0 +1,13 @@
+export const ShopWineModal = () => {
+  return (
+    <>
+      <div>
+        <img src="../images/wine_image.png" alt="wine image" />
+        <title></title>
+        <p></p>
+        <div></div>
+        <button>Add to card</button>
+      </div>
+    </>
+  );
+};
