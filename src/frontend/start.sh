1584ef992d2f8a0c5dc193e962256aeef8eacedd
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-06-02 16:10:01 +0200

    update backend

--- src/frontend/start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cd /frontend
-rm -rf node_modules package-lock.json
-npm install
-
-npm run dev
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: [
-      'matetask.uksouth.cloudapp.azure.com'
-    ]
-  }
+    allowedHosts: 'all'
+  },
 })
