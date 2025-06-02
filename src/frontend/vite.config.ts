ad8a2237d89e3e8a3adad3145306ad095f9539c3
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-06-02 15:50:56 +0200

    update backend

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: 'all',
     host: '0.0.0.0',
-    port: 80
+    port: 8080
   }
 })
