17142be8981ae6eddaa96119bd75854314edba7f
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-06-10 16:23:46 +0200

    update backend

--- install-app-backend.sh
+++ install-app-backend.sh
@@ -2,9 +2,12 @@
 
 sudo apt-get update -yq
 sudo apt-get install python3-pip -yq
+echo -------------------
 sudo mkdir /backend
 git clone -b develop https://github.com/Java50KhrypunovMaxim/shop_wine.git
 sudo cp -r shop_wine/* /backend
+
+echo ------------------
 sudo chmod 777 /backend/*
 sudo mv /backend/backend.service /etc/systemd/system/
 sudo systemctl daemon-reload
